33965d6d778b743e34cc5b70f9a010cfe1cd3d54
caijda <145417979+caijda@users.noreply.github.com>
2023-10-26 21:49:31 -0600

    Bug fixes

--- src/com/coderscampus/Game.java
+++ src/com/coderscampus/Game.java
@@ -13,24 +13,22 @@ public class Game {
 		Scanner scanner = new Scanner(System.in);
 		int guess = scanner.nextInt();
 
-		while (turn < 6) {
-			if (turn < 5) {
+		while (true) {
+			if (turn < 4 || guess == random) {
 				if (guess < 1 || guess > 100) {
 					System.out.println("Your guess is not between 1 and 100, please try again");
 					guess = scanner.nextInt();
+				} else if (guess < random) {
+					System.out.println("Please pick a higher number");
+					guess = scanner.nextInt();
+					turn++;
+				} else if (guess > random) {
+					System.out.println("Please pick a lower number");
+					guess = scanner.nextInt();
+					turn++;
 				} else {
-					if (guess < random) {
-						System.out.println("Please pick a higher number");
-						guess = scanner.nextInt();
-						turn++;
-					} else if (guess > random) {
-						System.out.println("Please pick a lower number");
-						guess = scanner.nextInt();
-						turn++;
-					} else {
-						System.out.println("You win!");
-						break;
-					}
+					System.out.println("You win!");
+					break;
 				}
 			} else {
 				System.out.println("You lose, the nubmer to guess was " + random);
